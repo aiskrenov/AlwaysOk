2ba58f1c3a3d0e131e0c865f033558da1743f641
Alexander Iskrenov <a.iskrenov@gmail.com>
2025-01-03 11:37:20 +1100

    Initial commit

---
+++ AlwaysOk/Controllers/AnyController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace AlwaysOk.Controllers;
+
+[ApiController]
+[Route("{*path}")]
+public class AnyController : ControllerBase
+{
+    private readonly ILogger<AnyController> _logger;
+
+    public AnyController(ILogger<AnyController> logger) => _logger = logger;
+
+    [HttpGet]
+    [HttpPost]
+    [HttpPut]
+    [HttpDelete]
+    [HttpPatch]
+    [HttpHead]
+    [HttpOptions]
+    public async Task<ActionResult> Action()
+    {
+        using var reader = new StreamReader(Request.Body);
+        var body = await reader.ReadToEndAsync();
+
+        _logger.LogInformation("""
+            Incoming Request
+            Scheme: {scheme}
+            Method: {method} 
+            Path: {path}
+            Query: {query}
+            Headers: {headers}
+            Body: {body}
+            """,
+            Request.Scheme, Request.Method, Request.Path, Request.QueryString,
+            JsonSerializer.Serialize(Request.Headers),
+            string.IsNullOrEmpty(body) ? "n/a" : body);
+
+        return Ok();
+    }
+}
